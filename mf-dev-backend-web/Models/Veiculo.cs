ca6393bf900b2524f9967c836d764e27732c710e
JoaoLopes994 <joaolsilva994@gmail.com>
2025-10-15 12:35:40 -0300

    config do banco de dados e tabela Veículos

---
+++ mf-dev-backend-web/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_web.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Obrigatório informar o Nome.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar a placa.")]
+        public string Placa { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o modelo do veículo.")]
+        public int AnoFabricacao { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o ano do veículo.")]
+        public int AnoModelo { get; set; }
+    }
+}
